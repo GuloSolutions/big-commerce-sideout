b36c8d9343526f803bba8b599ff889a648473c68
radboris <radboris@goldmember.gulosolutions.com>
2017-09-13 14:04:53 -0500

    WIP: composer updates

--- Configs.php
+++ Configs.php
@@ -1,7 +1,6 @@
 <?php
-
 namespace App;
 
-    define("store_id", $store_id);
-    define("token", $token);
-    define("id", $client_id);
+define("store_id", $store_id);
+define("token", $token);
+define("id", $client_id);
