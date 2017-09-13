c92e97c995a23e500551970e02d11f55a25fa821
radboris <radboris@goldmember.gulosolutions.com>
2017-09-13 14:04:33 -0500

    renamed primary class

--- BigCommerceWordpress.php
+++ BigCommerceWordpress.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App;
 
 require 'vendor/autoload.php';
@@ -10,21 +9,21 @@ use GuzzleHttp\Stream\Stream;
 use Bigcommerce\Api\Connection;
 use Bigcommerce\Api\Client as Bigcommerce;
 
-class BigCommerceSideout {
-
+class BigCommerceWordpress
+{
     protected $store_id;
     protected $token;
     protected $id;
 
-    public function __construct($client_id, $token, $store_id){
-
-            $this->token = $token;
-            $this->store_id = $store_id;
-            $this->id = $client_id;
+    public function __construct($client_id, $token, $store_id)
+    {
+        $this->token = $token;
+        $this->store_id = $store_id;
+        $this->id = $client_id;
     }
 
-    public function sendRequest() {
-
+    public function sendRequest()
+    {
         Bigcommerce::configure(array(
             'client_id' => $this->id,
             'auth_token' => $this->token,
@@ -34,10 +33,5 @@ class BigCommerceSideout {
         $products  = Bigcommerce::getProducts();
 
         return $products;
-
     }
 }
-
-
-
-
