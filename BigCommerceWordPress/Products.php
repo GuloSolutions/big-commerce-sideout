4b48bf91b6e75d2c72a1b7fa86ce1ce7fafe1c5d
radboris <radboris@goldmember.gulosolutions.com>
2017-09-20 16:12:30 -0500

    working stash that can be set

--- BigCommerceWordPress/Products.php
+++ BigCommerceWordPress/Products.php
@@ -7,43 +7,54 @@ use Bigcommerce\Api\Connection;
 use Bigcommerce\Api\Client as Bigcommerce;
 use Stash;
 
-class Products
+
+class Products extends Request
 {
     public $driver;
     public $pool;
+    public $cache;
 
     public function __construct()
     {
         $this->driver = new Stash\Driver\FileSystem(array());
         $this->pool = new Stash\Pool($this->driver);
+        $this->cache = true;
     }
 
     public function getProducts()
     {
-        $item = $this->pool->getItem('results');
+        if ($this->returnCache() === true)  {
+            $item = $this->pool->getItem('results');
 
-        $data = $item->get();
+            $data = $item->get();
 
-        if($item->isMiss()){
+            if($data === false){
+                $data = Bigcommerce::getProducts();
+                $this->pool->save($item->set($data));
+            }
+            return $data;
+        } else {
             $data = Bigcommerce::getProducts();
-            $this->pool->save($item->set($data));
+            return $data;
         }
-
-        return $data;
     }
 
     public function getFeaturedProducts()
     {
-        $item = $this->pool->getItem('featured_results');
+        if ($this->returnCache() === true)  {
+            $item = $this->pool->getItem('featured_results');
 
-        $data = $item->get();
+            $data = $item->get();
 
-         if($item->isMiss()){
+            if($data === false){
+                $data = Bigcommerce::getProducts(["is_featured" => "true"]);
+                $this->pool->save($item->set($data));
+            }
+            return $data;
+        } else {
             $data = Bigcommerce::getProducts(["is_featured" => "true"]);
-            $this->pool->save($item->set($data));
+            return $data;
         }
-
-        return $data;
     }
 
     public function getSetNumberProducts(integer $length)
@@ -55,5 +66,19 @@ class Products
 
         $products  = Bigcommerce::getProducts($filter);
         return $products;
+    }
+
+    public function setCache($value)
+    {
+        if($value === "true"){
+            $this->cache = true;
+        } else {
+            $this->cache = false;
+        }
+    }
+
+    public function returnCache()
+    {
+        return $this->cache;
     }
 }
