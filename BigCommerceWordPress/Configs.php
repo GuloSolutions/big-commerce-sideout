156016700bde7ba24b8512812cdf4043eb3b4225
radboris <radboris@goldmember.gulosolutions.com>
2017-09-21 11:40:15 -0500

    update phpunit and add namespace to configs

--- BigCommerceWordPress/Configs.php
+++ BigCommerceWordPress/Configs.php
@@ -1,11 +1,12 @@
 <?php
+namespace BigCommerceWordPress;
 
 class Configs
 
 {
-    const BIG_COMMERCE_SO_STORE_ID = 'your_credentials';
+    const BIG_COMMERCE_SO_STORE_ID = 'your_token';
 
-    const BIG_COMMERCE_SO_TOKEN = 'your_credentials';
+    const BIG_COMMERCE_SO_TOKEN = 'your_token';
 
-    const BIG_COMMERCE_SO_CLIENT_ID = 'your_credentials';
+    const BIG_COMMERCE_SO_CLIENT_ID = 'your_token';
 }
