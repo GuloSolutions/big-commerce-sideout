f07616cd01ba4acb1dfcf3e778f6afb51d7f3143
radboris <radboris@goldmember.gulosolutions.com>
2017-09-22 12:44:50 -0500

    see if naming fields diff will make a difference

--- BigCommerceWordPress/Configs.php
+++ BigCommerceWordPress/Configs.php
@@ -4,9 +4,9 @@ namespace BigCommerceWordPress;
 class Configs
 
 {
-    const BIG_COMMERCE_SO_STORE_ID = 'your_token';
+    const BIG_COMMERCE_SO_STORE_ID = 'store_id';
 
-    const BIG_COMMERCE_SO_TOKEN = 'your_token';
+    const BIG_COMMERCE_SO_TOKEN = 'token';
 
-    const BIG_COMMERCE_SO_CLIENT_ID = 'your_token';
+    const BIG_COMMERCE_SO_CLIENT_ID = 'id';
 }
