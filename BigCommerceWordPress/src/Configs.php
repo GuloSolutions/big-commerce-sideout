364337f99a5ea50df20d65b22e07f4894111c6ca
radboris <radboris@goldmember.gulosolutions.com>
2017-09-26 16:47:49 -0500

    add forward slash

--- BigCommerceWordPress/src/Configs.php
+++ BigCommerceWordPress/src/Configs.php
@@ -10,7 +10,7 @@ class Configs
 
     public function load()
     {
-    	$file = __DIR__ . 'parameters.yml';
+    	$file = __DIR__ . DIRECTORY_SEPARATOR . 'parameters.yml';
     	$data = spyc_load_file($file);
     	return $data;
     }
